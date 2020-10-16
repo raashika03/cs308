4bfe6d4d5c639777fe741a5409c6bf6bbb797bd5
Rashika Rathi <rashikasinha32@gmail.com>
2020-10-16 23:24:21 +0530

    adding DEBUG as prefix

--- sqrt.c
+++ sqrt.c
@@ -8,20 +8,19 @@ positive number. */
 int main(int argc, char* argv[]) {
 	
 	if (argc != 2) {
-		printf("Usage: sqrt input\n\n");
+		printf("DEBUG:Usage: sqrt input\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
 	if(input>0)
 	{
-	printf("Sqrt of %d is %f\n",input,sqrt(input));
+	printf("DEBUG:Sqrt of %d is %f\n",input,sqrt(input));
 	}
 	else{
-	printf("NEGSUPPORT:");
-	printf("Sqrt of %d is %fi",input,sqrt(-(input)));		
+	printf("DEBUG:Sqrt of %d is %fi",input,sqrt(-(input)));		
 	}
-	printf("End of the program.Exiting\n");
+	printf("DEBUG:End of the program.Exiting\n");
 	return(0);
 
 } // end main
