223946a9fdc1bc723ee0d838c6bd4c71969c1962
Rashika Rathi <rashikasinha32@gmail.com>
2020-10-16 23:22:23 +0530

    including NEGSUPPORT as prefix in every printf statement

--- sqrt.c
+++ sqrt.c
@@ -8,20 +8,19 @@ positive number. */
 int main(int argc, char* argv[]) {
 	
 	if (argc != 2) {
-		printf("Usage: sqrt input\n\n");
+		printf("NEGSUPPORT:Usage: sqrt input\n\n");
 		exit(-1);
 	}
 
 	int input = atoi(argv[1]);
 	if(input>0)
 	{
-	printf("Sqrt of %d is %f\n",input,sqrt(input));
+	printf("NEGSUPPORT:Sqrt of %d is %f\n",input,sqrt(input));
 	}
 	else{
-	printf("NEGSUPPORT:");
-	printf("Sqrt of %d is %fi",input,sqrt(-(input)));		
+	printf("NEGSUPPORT:Sqrt of %d is %fi",input,sqrt(-(input)));		
 	}
-	printf("End of the program.Exiting\n");
+	printf("NEGSUPPORT:End of the program.Exiting\n");
 	return(0);
 
 } // end main
